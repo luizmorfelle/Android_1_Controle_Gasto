9a016a07bf70101e081269f7b22206f60f517ed4
Luiz Felipe Cipriani Morfelle <luiz.morfelle@edu.univali.br>
2022-08-05 21:18:12 -0300

    First Study Project

---
+++ app/src/main/java/com/univali/projeto1_gastoviagem/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.univali.projeto1_gastoviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.univali.projeto1_gastoviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+        
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+
+        binding.textCost.text = "R$ ${"%.2f".format(totalValue)}"
+    }
+}
